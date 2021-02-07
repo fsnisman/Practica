7a12389006689af9e33a8531091ea8eae604be95
Nisman <fsnisman@gmail.com>
2021-02-08 01:47:31 +0300

    Третья практика

---
+++ 3/4 задание/Practica3.2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+/*
+===========================================================================================================
+Четвертое задание
+===========================================================================================================
+*/
+
+extern int a;
+extern int b;
+extern int c;
+extern int d;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    float f = static_cast <float> (c) / d;
+    f = static_cast <float> (b) + f;
+    f = static_cast <float> (a) * f;
+
+
+    cout << "Получаем число: " << f << endl;
+}
+
+
