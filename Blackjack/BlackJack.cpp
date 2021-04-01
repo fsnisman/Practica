ea9d7f011a53ecb2d58cd9e6f551390c2cf948cc
Alexander Nisman <fsnisman@gmail.com>
2021-04-01 18:37:38 +0300

    Update Blackjack

--- Blackjack/BlackJack.cpp
+++ Blackjack/BlackJack.cpp
@@ -2,70 +2,93 @@
 #include <cstdlib>
 using namespace std;
 //======================================================================================================================
-enum Suit{
-    Card_Peaks = 1,
-    Card_Hearts = 2,
-    Card_Diamonds = 3,
-    Card_Clubs = 4
-};
-enum Rank{
-    Card_Value2 = 2,
-    Card_Value3 = 3,
-    Card_Value4 = 4,
-    Card_Value5 = 5,
-    Card_Value6 = 6,
-    Card_Value7 = 7,
-    Card_Value8 = 8,
-    Card_Value9 = 9,
-    Card_Value10 = 10,
-    Card_Jack = 2,
-    Card_Lady = 3,
-    Card_King = 4,
-    Card_Ace = 1 | 11,
-};
-//======================================================================================================================
 class Cards{
-protected:
+private:
     Rank m_Rank;
     Suit m_Suit;
-    bool IsFaceUp;
-    int source = 0;
+    bool m_IsFaceUp;
 public:
-    bool m_IsFaceUp() const{            // 0 - рубашкоой, 1 - лицо
-        if (IsFaceUp == 0){
-            return IsFaceUp = 0;
-        }
-        else (IsFaceUp == 1){
-            return  IsFaceUp = 1;
-        }
-    }
+    enum Suit{
+        SPADES, HEARTS, DIAMONDS, CLUBS
+    };
+    enum Rank{
+        ACE = 1, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN,
+        JACK, QUEEN, KING
+    };
+    Card(rank r, suit s, bool ifu): m_Rank(r), m_Suit(s), m_IsFaceUp(ifu){}
     int getValue() const{
+        //если карта перевернута лицом вниз, ее значение равно 0
+        int value = 0;
+        if (m_IsFaceUp){
+            //значение на карте
+            value = m_Rank;
+            //значение равно для 10 JACK, QUEEN, KING
+            if (value > 10){
+                value = 10;
+            }
+        }
         return value;
     }
     void Flip(){
-        if (IsFaceUp == 0){
-            return IsFaceUp = 1;
-        }
-        if (IsFaceUp == 1){
-            return IsFaceUp = 0;
-        }
+        m_IsFaceUp = !(m_IsFaceUp)
     }
 };
 class Hand{
-private:
+protected:
     vector<Card*> m_Cards;
 public:
-    void Add(Card* pCard){
-        m_Cards.push_back(pCard);
-
+    Hand(){
+        m_Cards.reserve(7)
     };
+    //виртуальный деструктор
+    virtual ~Hand(){
+        Clear()
+    };
+    //добавляет карту в руку
+    void Add(Card* pCard){
+        m_Cards.push_back(pCard)
+    };
+    //очишает руку от карт
     void Clear(){
+        //проходит по векртору, освобождая память
+        vector<Card*>::iterator iter = m_Cards.begin();
+        for (iter = m_Cards.begin(); iter != m_Cards.end(); ++iter)
+        {
+            delete *iter;
+            *iter = 0;
+        }
+        //очищает вектор указатлей
         m_Cards.clear();
     };
     int getTotal(){
-        m_Rank += source;
-        return source;
-    }
+        //если карт в руке нет, возращает значение 0
+        if (m_Cards.empty()){
+            return 0;
+        }
+        if (m_Cards[0]->GetValue() == 0){
+            return 0;
+        }
+        //находим сумму очков всех карт, каждый туз дает 1 очко
+        int total = 0;
+        vector<Cards*>::const_iterator iter;
+        for (iter = m_Cards.bagin(); iter != m_Cards.end(); ++iter){
+            total += (*iter)->GetValue();
+        }
+        //определяет, держит ли рука туз
+        bool containsAse = false;
+        for (iter = m_Cards.begin(); iter != m_Cards.eds(); ++iter){
+            if ((*iter)->GetValue() == ACE){
+                containsAse = true;
+            }
+        }
+        //если рука держит туз и сумма довольно маленькая, туз дает 11 очков
+        if (containsAse && total <= 11){
+            //добавляем только 10 очков, поспольку мы уже добавли
+            //за каждый туз по одному очку
+            total += 10;
+        }
+        return total;
+    }                                   //получает сумму очков карт в руке. присваеваем значение тузу 1 или 11 по ситуаци
 };
 class Deck: public Hand{
 public:
@@ -84,14 +107,26 @@ public:
 };
 class GenericPlayer: public Hand{
 private:
-    string m_Name;
+    string Name;
 public:
-    virtual bool IsHitting() const = 0;
+    virtual bool IsHitting() const{
+        if (total < 21){
+            Add(Cards* total);
+            return total;
+        }
+    };
     bool IsBoosted() const{
-
+        if (total > 21){
+            return false;
+        }
+        else {
+            return bool;
+        }
     };
     void Bust() const{
-
+        if (IsBoosted() == false) {
+            cout << Name << " " << "Enumeration of cards" << endl;
+        }
     };
 };
 class Player: public GenericPlayer{
